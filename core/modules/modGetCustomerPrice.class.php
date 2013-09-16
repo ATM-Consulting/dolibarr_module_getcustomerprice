7f5020685f8cb3987ed5cae90eef0b2b09eb8ae4
Maxime Kohlhaas <maxime@atm-consulting.fr>
2013-09-16 09:22:47 +0200

    ID module + trad

--- core/modules/modGetCustomerPrice.class.php
+++ core/modules/modGetCustomerPrice.class.php
@@ -44,7 +44,7 @@ class modGetCustomerPrice extends DolibarrModules
 
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 232000;
+		$this->numero = 104010;
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'getcustomerprice';
 
@@ -54,7 +54,7 @@ class modGetCustomerPrice extends DolibarrModules
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
-		$this->description = "Utilise les derniers prix pratiqués pour le client";
+		$this->description = "Utilise le dernier prix pratiqué pour le client au lieu du prix catalogue";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = '1.1';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
