e872a198c5ad0c80fab15563721cd836c0fc318a
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-07-11 13:25:37 +0200

    Correction si on est sur un avoir => prix * -1

--- core/triggers/interface_99_modGetCustomerPrice_GetCustomerPriceWorkflow.class.php
+++ core/triggers/interface_99_modGetCustomerPrice_GetCustomerPriceWorkflow.class.php
@@ -109,6 +109,15 @@ class InterfaceGetCustomerPriceWorkflow
 			$TInfos = $this->_getLastPriceForCustomer($object);
 			
 			if(!empty($TInfos) && !empty($TInfos['prix'])) {
+				// Fonctionnement spécifique si on est sur une ligne d'avoir
+				if($object->element == 'facturedet') {
+					$f = new Facture($db);
+					$f->fetch($object->fk_facture);
+					if($f->type == 3) {
+						$TInfos['prix'] *= -1;
+					}
+				}
+				
 				$tabprice=calcul_price_total(
 					$object->qty,
 					(strpos($conf->global->GETCUSTOMERPRICE_WHATTOGET,'price') !== false) ? $TInfos['prix'] : $object->subprice,
